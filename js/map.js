0a1042955c2103bfd5905c145acd267a58dbace9
chr1s405 <christian.arthurmens@gmail.com>
2025-02-12 23:09:14 +0100

    made the map work with offset positions

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
-let windowWidth;
-let windowHeight;
+//initialize
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
 
 const overworldMap = document.getElementById("overworldMap");
+let minWidth = overworldMap.offsetLeft;
+let minHeight = overworldMap.offsetTop;
 let maxWidth = overworldMap.clientWidth;
 let maxHeight = overworldMap.clientHeight;
-//console.log(`width: ${maxWidth}\nheight: ${maxHeight}`);
 
 const character = document.getElementById("character");
 const characterWidth = character.clientWidth;
@@ -12,23 +14,35 @@ const characterHeight = character.clientHeight;
 let characterX = Number(character.style.left.substring(0,character.style.left.indexOf("px")));
 let characterY = Number(character.style.top.substring(0,character.style.top.indexOf("px")));
 const characterSpeed = 50;
+ScrollMap();
 
-resizeWindow();
 
-document.addEventListener("keydown", (e)=>{
+//eventlisteners
+addEventListener("keydown", (e)=>{
     if(e.keyCode === 87/*w*/){ moveY(-characterSpeed);};
     if(e.keyCode === 65/*a*/){ moveX(-characterSpeed);};
     if(e.keyCode === 83/*s*/){ moveY( characterSpeed);};
     if(e.keyCode === 68/*d*/){ moveX( characterSpeed);};
-    window.scroll(characterX - windowWidth/2, characterY - windowHeight/2);
-    //console.log(`( ${characterX} , ${characterY})`)
+    if(e.keyCode === 85/*u*/){ DebugInfo();};
+    ScrollMap();
 })
-addEventListener("resize", resizeWindow);
+addEventListener("resize", ()=>{
+    resizeWindow();
+    ScrollMap();
+})
+
+//functions
 function resizeWindow(){
     windowWidth  = window.innerWidth;
     windowHeight = window.innerHeight;
-    window.scroll(characterX - windowWidth/2, characterY - windowHeight/2);
-    //console.log(`windowWidth: ${windowWidth}\nwindowHeight: ${windowHeight}`);
+    cameraLeftBorder = minWidth + windowWidth / 2;
+    cameraTopBorder = minHeight + windowHeight / 2;
+}
+
+function ScrollMap(){
+    let x = minWidth + characterX - windowWidth/2;
+    let y = minHeight + characterY - windowHeight/2;
+    window.scroll(Math.max(minWidth, x), Math.max(minHeight, y));
 }
 function moveX(x){
     characterX += x;
@@ -49,4 +63,10 @@ function moveY(y){
         characterY = maxHeight - characterHeight;
     }
     character.style.top = `${characterY}px`;
+}
+function DebugInfo(){
+    console.clear()
+    console.log(`window:\nwidth: ${windowWidth}\nheight: ${windowHeight}`);
+    console.log(`overworld map:\nx: ${minWidth}\ny: ${minHeight}\nwidth: ${maxWidth}\nheight: ${maxHeight}`);
+    console.log(`character:\nx: ${characterX}\ny: ${characterY}`)
 }
