df11f49c751536d2dcacc0dd7cc44d6e26ffdb91
chr1s405 <christian.arthurmens@gmail.com>
2025-02-21 18:26:39 +0100

    added a simple menu for the backpack

--- js/backpack.js
+++ js/backpack.js
@@ -1,12 +1,19 @@
 const backpackIcon = document.getElementById("backpackIcon");
+const backpackMenu = document.getElementById("backpackMenu")
+const backpackCloseBtn = document.getElementById("backpackMenuCloseBtn")
 backpackIcon.addEventListener("click",(e)=>{
     openMenu();
+})
+backpackCloseBtn.addEventListener("click",(e)=>{
+    CloseMenu();
 })
 
 function openMenu(){
     backpackIcon.style.display = "none";
+    backpackMenu.style.display = "block";
     
 }
 function CloseMenu(){
     backpackIcon.style.display = "block";
+    backpackMenu.style.display = "none";
 }
