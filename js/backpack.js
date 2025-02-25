7286abca3d459f23d61c148d1d57ea5df13f9f83
chr1s405 <christian.arthurmens@gmail.com>
2025-02-25 23:03:34 +0100

    buttons in the menu can open event screens now

--- js/backpack.js
+++ js/backpack.js
@@ -1,19 +1,39 @@
 const backpackIcon = document.getElementById("backpackIcon");
 const backpackMenu = document.getElementById("backpackMenu")
 const backpackCloseBtn = document.getElementById("backpackMenuCloseBtn")
-backpackIcon.addEventListener("click",(e)=>{
+const backpackMenuItems = document.getElementsByClassName("backpackMenuBtn");
+const menuEvents = document.getElementsByClassName("menuEvent");
+
+backpackIcon.addEventListener("click", (e) => {
     openMenu();
 })
-backpackCloseBtn.addEventListener("click",(e)=>{
+backpackCloseBtn.addEventListener("click", (e) => {
     CloseMenu();
 })
+for (let i = 0; i < backpackMenuItems.length - 1; i++) {
+    backpackMenuItems[i].addEventListener("click", (e) => {
+        openEvent();
+        menuEvents[i].style.display = "block";
 
-function openMenu(){
+    });
+    menuEvents[i].children[0].addEventListener("click",(e)=>{
+        menuEvents[i].style.display = "none";
+        closeEvent();
+    })
+}
+
+function openMenu() {
     backpackIcon.style.display = "none";
     backpackMenu.style.display = "grid";
-    
+
 }
-function CloseMenu(){
+function CloseMenu() {
     backpackIcon.style.display = "block";
     backpackMenu.style.display = "none";
+}
+function openEvent(){
+    backpackMenu.style.display = "none"
+}
+function closeEvent(){
+    backpackIcon.style.display = "block"
 }
