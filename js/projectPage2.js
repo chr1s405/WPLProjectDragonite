735f998c034ab360862115da24d70f635d952f0c
chr1s405 <christian.arthurmens@gmail.com>
2025-03-15 13:45:48 +0100

    the logo background can now also be set trough js

--- js/projectPage2.js
+++ js/projectPage2.js
@@ -3,16 +3,13 @@ const gameOptions = document.getElementsByClassName("game_options");
 const background = document.getElementsByTagName("main")[0];
 const mainBtn = document.getElementById("game_mainBtn");
 const arrowBtns = document.getElementsByClassName("arrowBtn");
-const logos = Array.from(document.getElementsByClassName("game_options")).map(
-    (selectionDiv) => selectionDiv.children[0].src
-);
-const backgroundUrls = [
-    "../images/Magic-Background.jpeg",
-    "../images/yellow-lego-background.jpeg",
-    "../images/lordOfTheRingsbackground.jpeg",
-    "../images/fortniteBackground2.jpeg",
-    "../images/FIFA-Background.jpg",
-    "../images/pokemonBackground.jpeg",
+const images = [
+    {background: "../images/Magic-Background.jpeg", logo: "../images/magic.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
+    {background: "../images/yellow-lego-background.jpeg", logo: "../images/legoMasters.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
+    {background: "../images/lordOfTheRingsbackground.jpeg", logo: "../images/lordOfTheRings.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
+    {background: "../images/fortniteBackground2.jpeg", logo: "../images/fortnite.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
+    {background: "../images/FIFA-Background.jpg", logo: "../images/fifa.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
+    {background: "../images/pokemonBackground.jpeg", logo: "../images/pokempon-logo.jpeg", logoBgColor: "rgba(0, 0, 0, 0.5)"},
 ];
 
 arrowBtns[0].addEventListener("click", () => {
@@ -22,6 +19,7 @@ arrowBtns[1].addEventListener("click", () => {
     changeActive((activeIndex + 1) % gameOptions.length)
 });
 for ( let i = 0; i < gameOptions.length; i++){
+    gameOptions[i].style.backgroundColor = images[i].logoBgColor;
     gameOptions[i].addEventListener("click", ()=>{
         changeActive(i);
     })
@@ -40,6 +38,7 @@ function changeActive(index) {
     gameOptions[activeIndex].classList.remove("active");
     gameOptions[index].classList.add("active");
     activeIndex = index;
-    background.style.backgroundImage = `url(${backgroundUrls[activeIndex]}`;
-    mainBtn.style.backgroundImage = `url(${logos[activeIndex]}`;
+    background.style.backgroundImage = `url(${images[activeIndex].background}`;
+    mainBtn.style.backgroundImage = `url(${images[activeIndex].logo}`;
+    mainBtn.style.backgroundColor = `${images[activeIndex].logoBgColor}`;
 }
